60f3962e9649dd2a077a1556185adf90e23ef7a7
Olusegun Oladele Sofola <36111874+ssofola@users.noreply.github.com>
2024-03-10 19:17:41 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Edit this file with your own MongoDB URI
-    db: 'mongodb+srv://osofola1:IAYH9BN3NJgec8ys@mindquiz01.vykw8ur.mongodb.net/?retryWrites=true&w=majority&appName=MindQuiz01',
+    db: 'mongodb+srv://<username>:<password>@mindquiz01.vykw8ur.mongodb.net/?retryWrites=true&w=majority&appName=MindQuiz01',
     //db: 'mongodb://localhost/mindquiz',
     sessionSecret: 'SessionSecret',
     secretKey: 'real_secret'
