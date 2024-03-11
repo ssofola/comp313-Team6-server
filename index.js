3743450c2e0434015b182b25db7e50580293752d
Olusegun Sofola <ssofola@gmail.com>
2024-03-11 11:29:57 -0400

    "Olusegun's code update to standardize the code-base and extend the APIs to fulfill login and registration functionalities"

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const db = mongoose();
 
 // Create a new Express application instance
 const app = express();
-
+// enable a generic port instance
+const port = process.env.PORT || 5000;
 // Use the Express application instance to listen to the port 5000
-app.listen(5000);
+app.listen(port,  () => {
+    console.log(`Server running on port ${port}`)
+});
 
 module.exports = app;
 console.log('Server running at http://localhost:5000/');
